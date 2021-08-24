8adb5ee165b37c272fe6d069c0adb7f683ea628d
pieas-asif <pieas.asif4@gmail.com>
2021-08-24 22:51:28 +0600

    base variant in css, weather fetched

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var app = new Vue({
 	'el': '#app',
 	'data': {
+		'weather': {},
 		'time': '99.99',
 		'location': 'Current Locaiton',
 		'tip': 'If fire, git commit, git push, run for life',
@@ -9,25 +10,34 @@ var app = new Vue({
 		'lat': '',
 		'lon': '',
 		'error': '',
+		'appid': '4bcccb0e47f16ac96b8707ac00775626',
 	}, 
 	'methods': {
-		getCurrentPosition: function() {			
-			if(navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(this.setPosition);
-			} else {
-				this.error = "Geolocation is not supported."; 	 
-			}
-		},
-		setPosition: function(position) {
-			this.lat = position.coords.latitude;
-			this.lon = position.coords.longitude;
-		},
+		getCoordinates: function() {
+			return new Promise(function(resolve, reject) {
+				navigator.geolocation.getCurrentPosition(resolve, reject);
+			});
+	    },
 		fetchWeatherForCurrentLocation: async function()  {
-			this.getCurrentPosition();
-			// fetch('https://api.openweathermap.org/data/2.5/weather?lat='+ this.lat + "&lon=" + this.lon + "&appid=" + "4bcccb0e47f16ac96b8707ac00775626")
-			// .then(response => response.json())
-			// .then(commits => alert(commits));
-			// TODO: Do These
+			console.log(this.lat);
+			console.log(this.lon);
+			const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${this.appid}&units=metric`);
+			this.weather = response.data;
+			this.temp = Math.round(this.weather.main.temp);
+			this.location = this.weather.name;
+		},
+		getTime: function () {
+			var d = new Date();
+			let _hours = d.getHours();
+			let _minutes = d.getMinutes();
+			console.log(_hours);
+			console.log(_minutes);
 		}
+	}, 
+	mounted: async function() {
+		const position = await this.getCoordinates();
+    	this.lat = position.coords.latitude;
+    	this.lon = position.coords.longitude;
+    	await this.fetchWeatherForCurrentLocation();
 	} 
 });
