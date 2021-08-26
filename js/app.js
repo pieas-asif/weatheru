29dce424316cdcee9db12cac9d6eb4aa5c478006
Shaman Sharif <pieas.asif4@gmail.com>
2021-08-27 01:09:45 +0600

    TIP and IMG dynamically update

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ var app = new Vue({
 		'bgClass': 'base',
 		'location': 'Current Locaiton',
 		'tip': 'If fire, git commit, git push, run for life',
+		'weatherImg': "asset/svg/wind.svg",
 		'temp': '99',
 		'lat': '',
 		'lon': '',
@@ -19,7 +20,7 @@ var app = new Vue({
 				navigator.geolocation.getCurrentPosition(resolve, reject);
 			});
 	    },
-		fetchWeatherForCurrentLocation: async function()  {
+		fetchWeather: async function()  {
 			const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${this.appid}&units=metric`);
 			this.weather = response.data;
 			this.temp = Math.round(this.weather.main.temp);
@@ -27,13 +28,25 @@ var app = new Vue({
 			let weatherID = this.weather.weather[0].id;
 			if(weatherID >= 700 && weatherID <= 800) {
 				this.bgClass = 'sunny';
+				this.weatherImg = "asset/svg/sunny.svg";
+				this.tip = "wear sunscreen! lots of it!";
 			} else if (weatherID >= 200 && weatherID < 600) {
 				this.bgClass = 'rain';
+				this.weatherImg = "asset/svg/rain.svg";
+				this.tip = "take an umbrella when you go out";
 			} else if (weatherID >= 600 && weatherID < 700) {
 				this.bgClass = 'snow';
+				this.weatherImg = "asset/svg/snow.svg";
+				this.tip = "be sure to wear warm clothes"
 			} else if (weatherID > 800 && weatherID < 900) {
 				this.bgClass = 'base';
+				this.weatherImg = "asset/svg/wind.svg";
+				this.tip = "enjoy your day";
 			}
+		},
+		fetchWeatherForCurrentLocation: async function() {
+			await this.getCoordinates();
+			await this.fetchWeather();
 		},
 		getTime: function () {
 			var d = new Date();
@@ -53,14 +66,13 @@ var app = new Vue({
 		const position = await this.getCoordinates();
     	this.lat = position.coords.latitude;
     	this.lon = position.coords.longitude;
-    	await this.fetchWeatherForCurrentLocation();
+    	await this.fetchWeather();
 	},
 	beforeCreate: function () {
 	    setInterval(() => {
 	      	this.getTime();
 	    }, 1000);
 	    setInterval(async () => {
-	      	await this.getCoordinates();
 	      	await this.fetchWeatherForCurrentLocation();
 	    }, 1.8e+6);
 	  },
